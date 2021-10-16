3649f8f401703c7b43a990efba2b6bb16ac974f9
Nick <nfzaremba@gmail.com>
2021-10-16 16:27:04 -0700

    deploy

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3001;
 
 const initApp = () => {
   app.use(middlewares);
+
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", (_req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+
   app.use(router);
   app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
