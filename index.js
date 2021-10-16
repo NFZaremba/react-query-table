d0f367c87570a80fc9623c9c05f5e9e3a733135e
Nick <nfzaremba@gmail.com>
2021-10-15 17:32:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const app = jsonServer.create();
+const express = require("express");
+const path = require("path");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+
+const initApp = () => {
+  app.use(middlewares);
+  app.use(router);
+  app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+  });
+};
+
+initApp();
+
+// { static: "../client/build" }
