7552804f8f1cf47a9169327dc7db9813db400761
Nick <nfzaremba@gmail.com>
2021-10-17 11:47:08 -0700

    deploy

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = jsonServer.create();
 const express = require("express");
 const path = require("path");
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: "./client/build" });
 const port = process.env.PORT || 3001;
 
 const initApp = () => {
   app.use(middlewares);
 
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  // app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get("*", (_req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
+  // // All remaining requests return the React app, so it can handle routing.
+  // app.get("*", (_req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  // });
 
   app.use(router);
   app.listen(port, () => {
